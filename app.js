bdf6d862da69d0f4eea034da12f3ee3f070e715a
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-12 22:32:58 -0500

    adding employee information to app.js

--- app.js
+++ app.js
@@ -9,16 +9,101 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { inherits } = require("util");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+function Manager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "number",
+        },
 
+
+
+    ]);
+};
+
+function Engineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's ID?",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's GitHub username?",
+            name: "username",
+        },
+
+
+
+    ]);
+};
+
+function Intern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the intern's ID?",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "What is the intern's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What school does the intern attend?",
+            name: "school",
+        },
+
+
+
+    ]);
+};
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -35,3 +120,4 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+init(); 
